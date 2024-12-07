5915ef263d69f98d67b4aa43ed6a9eb3890e6ec4
lolcat <105110312+thelolcat@users.noreply.github.com>
2024-12-07 14:49:45 +0530

    Add colored text, bugfix
    Add colours for text, fix bug where the losing player is shown as having won, and comment code

--- tictactoe-src/tictactoe-src.cpp
+++ tictactoe-src/tictactoe-src.cpp
@@ -12,7 +12,7 @@ char ui[3][10] = { {' ',' ',' ','|',' ',' ',' ','|',' ',' '},
                    {' ',' ',' ','|',' ',' ',' ','|',' ',' '},
                    {' ',' ',' ','|',' ',' ',' ','|',' ',' '}, };
 
-char currentPlayer = 'X';
+char currentPlayer = 'O';
 int input = 0;
 bool checkWin();
 
@@ -25,7 +25,16 @@ int main()
         // draws UI
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 10; j++) {
-                cout << ui[i][j];
+                switch (ui[i][j]) {
+                case 'X':
+                    cout << "\033[31m" << ui[i][j] << "\033[0m"; // gives 'X' with the specified color (red)
+                    break;
+                case 'O':
+                    cout << "\033[32m" << ui[i][j] << "\033[0m"; // gives 'O' with the specified color (green)
+                    break;
+                default:
+                    cout << ui[i][j]; //gives all other characters with the specified color (default)
+                }
             }
             cout << endl;
         }
@@ -33,22 +42,21 @@ int main()
         // checks for a win
         if (checkWin()) {
             cout << currentPlayer << " won!\nPlay again?(y/n)";
+            //TODO triggers restart or closes game based on input
         }
 
-        // changes player
-        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
-
-        // gets input, modifies it such that '1' becomes 1 and so on,
+        // gets input, parses it from char to int,
         // and checks if the input is in the set 0 - 9
+        // TODO accept only one input per turn
         input = cin.get();
-        input -= 48;
+        input -= 48; // so that '1' becomes 1 and so on
         if (input < 1 || input > 9) continue;
 
-        // processes input and stores it in UI.
+        // processes input and stores it in the UI array.
         // Uses a unique algorithm to calculate the position in the UI to which each number corresponds to
         if (ui[(int)(9 - input)/3][(int)((input - 1) % 3) * 4 + 1] == ' ') {
+            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X'; // changes player
             ui[(int)(9 - input)/3][(int)((input - 1) % 3) * 4 + 1] = currentPlayer;
-            currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
         }
     }
 }
@@ -59,9 +67,14 @@ bool checkWin() {
     for (int i = 0; i < 3; i++) {
         if (ui[i][1] == ui[i][5] && ui[i][5] == ui[i][9] && ui[i][1] != ' ') return true;
     }
+
     // checks rows
     for (int i = 1; i <= 9; i += 4) {
         if (ui[0][i] == ui[1][i] && ui[0][i] == ui[2][i] && ui[0][i] != ' ') return true;
     }
+
+    // TODO checks diagonals
+
+
     return false;
 }
